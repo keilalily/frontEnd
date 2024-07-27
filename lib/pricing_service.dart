d39667baec018f7644eaab3ff24a7be004fe9914
Peter John <cabantogpeterjohnm@gmail.com>
2024-07-27 23:23:55 +0800

    Pricing Setting Done!

--- lib/pricing_service.dart
+++ lib/pricing_service.dart
@@ -2,21 +2,46 @@ import 'dart:convert';
 import 'package:frontend/config.dart';
 import 'package:http/http.dart' as http;
 
-class PricingService {
+// class PricingService {
 
-  Future<Map<String, String>> fetchPricingData() async {
-    final response = await http.get(Uri.parse('http://${AppConfig.ipAddress}:3000/pricing/prices'));
+  // Future<Map<String, String>> fetchPricingData() async {
+  //   final response = await http.get(Uri.parse('http://${AppConfig.ipAddress}:3000/pricing/prices'));
+  //   if (response.statusCode == 200) {
+  //     final pricing = jsonDecode(response.body);
+  //     return {
+  //       'longBondPrice': pricing['longBondPrice'] ?? '0',
+  //       'shortBondPrice': pricing['shortBondPrice'] ?? '0',
+  //       'coloredPrice': pricing['coloredPrice'] ?? '0',
+  //       'grayscalePrice': pricing['grayscalePrice'] ?? '0',
+  //       'highResolutionPrice': pricing['highResolutionPrice'] ?? '0',
+  //       'mediumResolutionPrice': pricing['mediumResolutionPrice'] ?? '0',
+  //       'lowResolutionPrice': pricing['lowResolutionPrice'] ?? '0',
+  //     };
+  //   } else {
+  //     throw Exception('Failed to load pricing data');
+  //   }
+  // }
+
+
+// Future<double> fetchPricing() async {
+//   final response = await http.get(Uri.parse('http://${AppConfig.ipAddress}:3000/pricing/prices'));
+
+//   if (response.statusCode == 200) {
+//     var data = json.decode(response.body);
+//     return data['price'];
+//   } else {
+//     throw Exception('Failed to load pricing');
+//   }
+// }
+// }
+
+class PricingService {
+  Future<Map<String, dynamic>> fetchPricingData() async {
+    final response = await http.get(
+      Uri.parse('http://${AppConfig.ipAddress}:3000/pricing/prices'),
+    );
     if (response.statusCode == 200) {
-      final pricing = jsonDecode(response.body);
-      return {
-        'longBondPrice': pricing['longBondPrice'] ?? '0',
-        'shortBondPrice': pricing['shortBondPrice'] ?? '0',
-        'coloredPrice': pricing['coloredPrice'] ?? '0',
-        'grayscalePrice': pricing['grayscalePrice'] ?? '0',
-        'highResolutionPrice': pricing['highResolutionPrice'] ?? '0',
-        'mediumResolutionPrice': pricing['mediumResolutionPrice'] ?? '0',
-        'lowResolutionPrice': pricing['lowResolutionPrice'] ?? '0',
-      };
+      return json.decode(response.body);
     } else {
       throw Exception('Failed to load pricing data');
     }
